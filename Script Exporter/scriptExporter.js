9764f08da893089408b126ef221b706010154a6a
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2026-04-24 09:34:53 -0500

    control
    base control system

--- Script Exporter/scriptExporter.js
+++ Script Exporter/scriptExporter.js
@@ -1,3 +1,18 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+    const mode = ns.args[0];
+
+    const SELF = ns.getScriptName();
+    const JSON_OUT = "scripts_dump.json";
+    const TXT_OUT = "scripts_dump.txt";
+
+    if (mode === "ex") {
+        await exportScripts(ns, SELF, JSON_OUT, TXT_OUT);
+    } 
+    else if (mode === "im") {
+        await importScripts(ns, SELF, TXT_OUT);
+    } 
+    else {
+        ns.tprint("Usage: run script.js [ex | im]");
+    }
 }
